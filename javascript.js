163b3e2d914aee0a73b1b731172efc5114d37a64
Nicolas Balverdi <92952545+NicolasBalverdi@users.noreply.github.com>
2023-03-01 10:13:43 -0300

    Comienzo

---
+++ javascript.js
@@ -0,0 +1,7 @@
+document.getElementById('mail').onclick=function copiarMail() {
+    console.log('mail copiado');
+}
+document.getElementById("cel").onclick=function copiarCel() {
+    console.log('numero copiado');
+}
+document.getElementById('mostrar').onclick=document.getElementById('info').style.display='block';
